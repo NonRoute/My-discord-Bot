d3d07ee4375d504dfcb260a5f6a1d4458d3ea0b6
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2022-02-03 12:00:36 +0000

    Make bot respond to sad words

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import discord
 import os
 import requests
 import json
+import random
+from replit import db
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -19,12 +29,16 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    msg = message.content
+    
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
     if message.content.startswith('$inspire'):
       quote = get_quote()
       await message.channel.send(quote)
+
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.environ['TOKEN'])
